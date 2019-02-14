9b2b6b3eb0ed46ffc1dfe995ba8c29286a56f934
phong lu <phong_lu@symantec.com>
2019-02-14 14:58:21 -0800

    initial check-in

---
+++ XWKWebViewDemo.iOS/XWKWebViewDemo.iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XWKWebViewDemo.iOS
+//
+//  Created by plu on 2/14/19.
+//  Copyright © 2019 plu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ XWKWebViewDemo.macOS/XWKWebViewDemo.macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XWKWebViewDemo.macOS
+//
+//  Created by plu on 2/14/19.
+//  Copyright © 2019 plu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
